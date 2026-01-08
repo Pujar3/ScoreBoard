c65717c5887e122b293efa724622ebbf3d2c3287
Puja Roy <pujar3@nycstudents.net>
2026-01-08 12:21:17 -0500

    Updated Scoreboard

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Main
+{
     public static void main(String[] args)
     {
         String info;
         Scoreboard game = new Scoreboard("Red", "Blue");
-                info = game.getScore();
+        info = game.getScore();
         System.out.println(info);
+//        game.recordPlay(0);
+//        info = game.getScore();
+//        System.out.println(info);
+//        game.recordPlay(3);
+//        info = game.getScore();
+//        System.out.println(info);
     }
 }
