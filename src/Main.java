68f0908c7a82172ad3017fde4d7a98ad004b2af6
Pujar3 <pujar3@nycstudents.net>
2026-01-12 21:39:18 -0500

    Added Test Cases

--- src/Main.java
+++ src/Main.java
@@ -26,8 +26,7 @@ public class Main
         game.recordPlay(0);
         info = game.getScore();
         System.out.println(info);
-        Scoreboard match =
-                new Scoreboard("Lions", "Tigers");
+        Scoreboard match = new Scoreboard("Lions", "Tigers");
         info = match.getScore();
         System.out.println(info);
         info = game.getScore();
